945651d5cf23c6a877a42bdffd2c80ba3d634254
vikash0406 <vikash.verma@infoneotech.com>
2025-08-05 12:54:29 +0530

    Create RegisterForm.java

---
+++ RegisterForm.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.apache.struts.action.ActionForm;
+
+public class RegisterForm extends ActionForm {
+    private String username;
+    private String password;
+    private String email;
+    private String city;
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+}
