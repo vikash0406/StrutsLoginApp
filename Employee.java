5acbaa4243d8d995de28610be11d4a9646e192e3
vikash0406 <vikash.verma@infoneotech.com>
2025-08-05 12:16:32 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String city;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+}
